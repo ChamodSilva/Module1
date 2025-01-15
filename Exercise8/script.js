431b21c40f08fb7f5f350df31a2ce5c46dc753dd
CharcoalZz <chamodsilva@gmail.com>
2025-01-15 21:57:10 +0800

    Added a cycle function to change opposing die faces, to make the roll more realistic!

--- Exercise8/script.js
+++ Exercise8/script.js
@@ -1,5 +1,29 @@
 //Dice result variable
 let result = 0;
+const d6_even_faces = [[6,1],[4,3],[2,5]];
+const d6_odd_faces = [[1,6],[3,4],[5,2]];
+
+function cycleD6(result)
+{
+    let current_faces;
+    if (result % 2 === 0)
+    {
+        current_faces = d6_even_faces;
+    }
+    else
+    {
+        current_faces = d6_odd_faces;        
+    }
+
+    while (result != parseInt(current_faces[0][0]))
+    {
+        const lastPair = current_faces.pop();
+        current_faces.unshift(lastPair);
+    }
+    
+    
+
+}
 
 function rollD6()
 {
@@ -12,8 +36,11 @@ function rollD6()
     //Generating number between and including the minRoll and maxRoll
     result = Math.floor(Math.random() * (maxRoll - minRoll +1)) + minRoll;
     
+    console.log(result)
+
     //Updating the result on the html page
     updateResult(result);
+    cycleD6(result);
 }
 
 function rollD10()
